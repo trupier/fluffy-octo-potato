1fe737636084e603b26b31651b6f5daaef3ab752
trupier <michal.konkolewski01158@gmail.com>
2023-03-04 05:08:29 +0100

    fast commit

--- fluffyoctopotato/articles/models.py
+++ fluffyoctopotato/articles/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    author_id = models.IntegerField(primary_key=True)
+    author_id = models.AutoField(primary_key=True)
     author_name = models.CharField(unique=True, max_length=100)
+    def __str__(self):
+        return f"Author: {self.author_name}, id: {self.author_id}"
